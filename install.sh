08a41d3677a8227226dc0b5a9cc16ae39cee8765
lliwi <31549684+lliwi@users.noreply.github.com>
2022-06-18 18:11:21 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#CarbonCopy
+sudo apt install osslsigncode -y
+pip3 install pyopenssl
+
+#impacket
+cd /opt/tools/ldap/impacket
+pip3 install -r requirements.txt
+python3 -m pip install impacket
+python3 -m pip install .
+sudo python3 setup.py install
+
+#kerbrute
+cd /opt/tools/ldap/kerbrute
+pip3 install -r requirements.txt
+sudo python3 setup.py install
+
+#koroni
+cd /opt/tools/mobile/koroni/
+chmod +x koroni
+
+#bettercap
+/opt/tools/network/bettercap
+make build
+sudo make install
+
+#sslstript
+cd /opt/tools/network/
+sudo cp sslstrip /usr/lib/go/src
+cd /usr/lib/go/src/sslstrip/cli 
+go build main.go
+
+#Vxscan
+cd /opt/tools/network/Vxscan
+pip3 install -r requirements.txt
+
+#hotspot bypass
+sudo apt install sipcalc -y
